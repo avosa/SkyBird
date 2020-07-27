fecd207292701c347d1ce4a53f6afc08361ad782
avosa <websterb17@gmail.com>
2020-07-28 00:59:59 +0300

    Added new files

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.clock import Clock
 
+from pipe import Pipe
 
 class Background(Widget):
     cloud_texture = ObjectProperty(None)
@@ -28,8 +29,8 @@ class Background(Widget):
 
     def scroll_textures(self, time_passed):
         # Update the uvpos of the texture
-        self.cloud_texture.uvpos = ( (self.cloud_texture.uvpos[0] - time_passed/2.0)%Window.width , self.cloud_texture.uvpos[1])
-        self.floor_texture.uvpos = ( (self.floor_texture.uvpos[0] - time_passed)%Window.width, self.floor_texture.uvpos[1])
+        self.cloud_texture.uvpos = ( (self.cloud_texture.uvpos[0] + time_passed/2.0)%Window.width , self.cloud_texture.uvpos[1])
+        self.floor_texture.uvpos = ( (self.floor_texture.uvpos[0] + time_passed)%Window.width, self.floor_texture.uvpos[1])
 
         # Redraw the texture
         texture = self.property('cloud_texture')
